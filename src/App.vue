3b6398089eb51750b4167cbfb60b968cb2bca9f6
Lucas Bogerd <l.bogerd@protonmail.com>
2019-11-10 22:59:53 +0100

    Added intro snippet

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,11 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://www.google.com"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-3">Word Lid</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,19 +2,27 @@
   <section>
     <v-parallax 
       src="../assets/sea1.jpg"
-      height="600"
+      height="400"
     >
-      <v-layout 
-        column
-        align-center
-        justify-center
-        class="white--text"
+      <v-layout
+        row
       >
-        <img 
-          src="../assets/logo.svg" 
-          alt="Logo" 
-          height="200"
+        <v-flex 
+          xs12 
+          sm6
         >
+          <v-card 
+            class="ma-5"
+            id="intro"
+          >
+            <v-card-title primary-title>Tabula Rasa</v-card-title>
+            <v-card-text class="mt-3 mb-5 black--text">
+              Tabula Rasa is een partij die oplossingsgericht te werk gaat voor plannen voor de lange termijn. Een nieuw begin is mogelijk, 
+              door middel van constructieve en pragmatische probleemoplossingen. Met onze ideeën zetten wij ons in voor naar een waardige 
+              toekomst voor mens en natuur.
+            </v-card-text>
+          </v-card>
+        </v-flex>
       </v-layout>
     </v-parallax>
   </section>
@@ -27,5 +35,11 @@ export default {
 </script>
 
 <style>
+#intro {
+  background-color: rgba(255, 255, 255, 0.85);
+}
 
+#intro > .v-card__title {
+  background-color: rgba(255, 255, 255);
+}
 </style>
