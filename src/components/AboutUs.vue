6621aa2780fb3652e7960173797541045c542a3b
Lucas Bogerd <l.bogerd@protonmail.com>
2019-11-19 02:46:33 +0100

    Added AboutUs tiles

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,26 +1,48 @@
 <template>
   <section>
-    <v-layout 
+    <v-layout
       column
       wrap
-      class="my-12"
       align-center
+      class="full"
     >
       <v-flex 
         xs10
+        class="bg"
       >
-        <h1>Over ons</h1>
-        <v-divider></v-divider>
-        <v-card>
-          <v-img
-            src="../assets/sea2.jpg"
+        <v-container grid-list-xl>
+          <v-layout 
+            row 
+            wrap 
+            align-center
           >
-
-          </v-img>
-          <v-card-title primary-title>
-            Waarden
-          </v-card-title>
-        </v-card>
+            <v-flex 
+              xs10
+              offset-xs1 
+              md5
+              offset-md1
+            >
+              <v-card>
+                <v-img 
+                  src="../assets/manifest-themes/climate1.jpg"
+                  height="200"
+                ></v-img>
+              </v-card>
+            </v-flex>
+            <v-flex 
+              xs10
+              offset-xs1 
+              md5
+            >
+              <v-card>
+                <v-img 
+                  src="../assets/manifest-themes/climate1.jpg"
+                  height="200"
+                ></v-img>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </v-flex>
     </v-layout>
   </section>
