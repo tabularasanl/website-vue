1157bda0e379c6b99a68b693033ea3929d6f3329
Lucas Bogerd <l.bogerd@protonmail.com>
2019-11-21 02:13:24 +0100

    Form as iframe as PoC

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <iframe 
+    src="https://docs.google.com/forms/d/e/1FAIpQLScppp5D2LapwqstkATaHBdwlJFvCRfEv3XJxzLx_WPuUa-vtg/viewform?embedded=true" 
+    width="640" 
+    height="721" 
+    frameborder="0" 
+    marginheight="0" 
+    marginwidth="0"
+    class="mt-10"
+    id="signup"
+  >
+    Loading…
+  </iframe>
+</template>
+
